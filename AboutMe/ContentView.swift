23500f1fc6f673985c1c56a5ad8c980056ccdd38
Claudine <claudinekhoriaty@gmail.com>
2025-07-23 15:12:04 -0400

    changed my idea completely lol

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -10,15 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(red: 255/255, green: 196/255, blue: 210/255)
+            Color(red: 0/255, green: 0/255, blue: 0/255)
                 .ignoresSafeArea ()
         VStack {
                 Text("ABOUT ME")
                 .font(.largeTitle)
-                 
+                .fontWeight(.semibold)
+                .foregroundColor(Color(hue: 0.892, saturation: 0.455, brightness: 0.996))
+                .multilineTextAlignment(.leading)
+                .padding(.bottom, 14.0)
+            
+            Image("Moi")
+                .resizable()
+                .padding()
+                .frame(width:300, height: 390)
+            
+                
+                
             }
+        .padding([.leading, .bottom, .trailing], 258.0)
         }
-        .padding()
     }
 }
 
